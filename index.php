3d2857278679b165501707f7a3d1e711cc5e6cd2
SyedSa <syedsaeedahmed01@gmail.com>
2017-05-29 11:20:59 -1200

    Practice started for php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>                            						
+<body>
+<?php>							
+
+	//variable
+$varb = 25;
+$varc = 5;
+$sum = $varb + $varc;
+	
+echo $sum;	
+
+					
+?>		
+</body>														
+</html>
